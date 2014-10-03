61eeae925173e708adc384c209d7543c12771ffc
Eder Roger Souza <eder.roger.souza@gmail.com>
2014-10-03 08:01:19 -0300

    change the database to mongo NOSQL and create a first scaffold

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,22 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+#require 'rails/all'
+
+require "action_controller/railtie"
+require "action_mailer/railtie"
+#require "active_resource/railtie" # Comment this line for Rails 4.0+
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+if defined?(Bundler)
+  Bundler.require(*Rails.groups)
+end
 
 module Colchonet
   class Application < Rails::Application
+    Mongoid.logger.level = Logger::DEBUG
+    Moped.logger.level = Logger::DEBUG
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
