cafb639c56c85add32a79294e53c4999fb71f00a
Eder Roger Souza <eder.roger.souza@gmail.com>
2014-10-09 14:20:46 -0300

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,19 @@ class User
   field :password, type: String
   field :location, type: String
   field :bio, type: String
+
+  EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
+  validates_presence_of :email, :full_name, :location, :password
+  validates_confirmation_of :password
+  validates_length_of :bio, minimun: 30, allow_blank: false
+  validate :email_format
+
+  private
+
+  def email_format
+    errors.add(:email, :invalid) unless  email.match(EMAIL_REGEXP)
+  end
+
+
 end
